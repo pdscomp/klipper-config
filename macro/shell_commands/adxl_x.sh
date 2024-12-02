bd49278d72b1180b101f4ebd3afa19dedadf005c
Paul Swenson <pdscomp@gmail.com>
2024-12-01 20:58:39 -0500

    Adding ADXL macros as per https://www.reddit.com/r/klippers/comments/x8jnff/macro_for_automated_input_shaper_evaluation/

---
+++ macro/shell_commands/adxl_x.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$HOME/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o $HOME/shaper_calibrate_x.png
---
+++ macro/shell_commands/adxl_y.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$HOME/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o $HOME/shaper_calibrate_y.png
